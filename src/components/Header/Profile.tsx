941e20cfc3a6575bf58f14507011f4dcc7b6fc1a
Vandson Falcão <vandson.falcao@inbec.com.br>
2022-08-11 16:39:43 -0300

    responsivity Profile

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export default function Profile() {
+interface ProfileProps {
+	showProfileData?: boolean;
+}
+
+export default function Profile({ showProfileData = true }: ProfileProps) {
 	return (
 		<Flex align="center">
-			<Box mr="4" textAlign="right">
-				<Text>Vandson Falcão</Text>
-				<Text color="gray.50" fontSize="small">
-					vandsonsf@gmail.com
-				</Text>
-			</Box>
+			{showProfileData && (
+				<Box mr="4" textAlign="right">
+					<Text>Vandson Falcão</Text>
+					<Text color="gray.50" fontSize="small">
+						vandsonsf@gmail.com
+					</Text>
+				</Box>
+			)}
 			<Avatar
 				size="md"
 				name="Vandson Falão"
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, useBreakpointValue } from "@chakra-ui/react";
 
 import Logo from "./Logo";
 import NotificationsNav from "./NotificationNav";
@@ -6,13 +6,17 @@ import Profile from "./Profile";
 import SearchBox from "./SearchBox";
 
 export default function Header() {
+	const isWideVersion = useBreakpointValue({
+		base: false,
+		lg: true,
+	});
 	return (
 		<Flex w="100%" as="header" maxW={1480} h="20" mx="auto" mt="4" px="6" align="center">
 			<Logo />
 			<SearchBox />
 			<Flex align="center" ml="auto">
 				<NotificationsNav />
-				<Profile />
+				<Profile showProfileData={isWideVersion} />
 			</Flex>
 		</Flex>
 	);
