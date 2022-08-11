34180c388c44d57a7e8fbe7fb7038f0c4e675155
Vandson Falcão <vandson.falcao@inbec.com.br>
2022-08-11 16:42:03 -0300

    responsivity Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -13,7 +13,7 @@ export default function Header() {
 	return (
 		<Flex w="100%" as="header" maxW={1480} h="20" mx="auto" mt="4" px="6" align="center">
 			<Logo />
-			<SearchBox />
+			{isWideVersion && <SearchBox />}
 			<Flex align="center" ml="auto">
 				<NotificationsNav />
 				<Profile showProfileData={isWideVersion} />
